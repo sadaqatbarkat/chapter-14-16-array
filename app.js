21fc2ee9cae80615f94c0f5584429e751a83d8d2
sadaqatbarkat <153889463+sadaqatbarkat@users.noreply.github.com>
2024-03-18 20:14:28 +0500

    add index and app.js files

---
+++ app.js
@@ -0,0 +1,94 @@
+//Q3
+// let names = ["Sadaqat ali","Saad ahmed", "Zain", "Imad"];
+// console.log(names);
+
+//Q4
+// let number = [0,1,2,3,4,5];
+// console.log(number);
+// console.log(number.length);
+//Q5
+// let trueFalse = [true,false];
+// console.log(trueFalse);
+
+//Q6
+// let mixarr=[ "flower" ,1,2,3, true,false,];
+// console.log(mixarr);
+
+//Q7
+// let qualification = ["SSC", +"HSC","BCS","BS","BCOM","MS","M.Phil","PhD"];
+
+// document.write(
+// "<table>"     
+//  +"<tr>" 
+//  +"<th>"+qualification   +"</th>" 
+//  + "</tr>"
+// +"<table"
+// )
+// Q8
+
+// let studentNames = ["ALi","Yasir" ,"Sajid"];
+// let scores = [320,230,480];
+// let per1 = 320/500*100
+// let per2 = 230/500*100
+// let per3 = 480/500*100
+
+// document.write(
+// "<table>"    
+// +"<tr>"+ 
+// "<td>" +"Score of ALi is 320. Percentage:"+ per1+"%" +"</td>"
+// +"</tr>"
+// +"<tr>"+ 
+// "<td>" +"Score of Yasir is 230. Percentage:"+per2+"%"  +"</td>"
+// +"</tr>"
+// +"<tr>"+ 
+// "<td>" +"Score of Sajid is 480. Percentage:"+per3+"%" +"</td>"
+// +"</tr>"
+
+// +"<table>"
+//)
+
+//Q10
+// let order = [ 320,230,480,120]
+// document.write( "Scores of Students : ",order +"<br>")
+// let assend = order.sort();
+// document.write( "Ordered Scores of Students : ",assend);
+
+//11
+// let cities = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"]
+// document.write("<h4>" +"Cities List:"+"</h4>"+ "<h4>"+ cities   +"</h4>"   );
+// let returns= cities.slice(2,4);
+// document.write("<h4>" +"Selected cities list:"+"</h4>"+"<h4>"  + returns  +"</h4>")
+
+
+//Q12
+// let arr = ["This","is","my","cat"];
+// document.write("<h2>" +"Array:" +"</h2>"  + "<h2>"+arr +"</h2>");
+// let returnValue=arr.join(" ");
+// document.write("<h2>" +"String: " +"</h2>"  +"<h2>"+returnValue +"</h2>")
+
+//   
+
+
+
+//Q15
+// let phones = ["Apple", "Samsung", "Motorola", "Nokia", "Sony", "Hairer"]
+// document.write(phones)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
